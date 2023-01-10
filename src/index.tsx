d6a3cafd24a0f8130d73a027dae86f7b6e8f991c
Andreas <andreas95560@gmail.com>
2023-01-10 20:05:19 +0100

    modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Modal from './components/modal/modal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Modal />
   </React.StrictMode>
 );
 
